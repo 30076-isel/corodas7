1413851ebaacfab5d5de33c5704fd2386321c611
30076-isel <a30076@alunos.isel.pt>
2015-11-29 13:43:58 +0000

    second commit

--- javascript/pictures.js
+++ javascript/pictures.js
@@ -1,1 +1,15 @@
-var x= [];
+function changePicture()
+{
+	
+	var date = new Date();
+	var hours = date.getHours();
+	if(hours > 7 && hours < 18)
+		document.body.style.backgroundImage = "url('ImagesBig/igrejadia.jpg')";
+	else
+		document.body.style.backgroundImage = "url('imagesBig/igrejanoite.jpg')";
+}
+
+window.onload = function()
+{
+	changePicture();
+}
